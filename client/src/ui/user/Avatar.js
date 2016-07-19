0432e0513108e6f8796c5011fb8a0631da0948ff
BeijiYang <langyuyang@outlook.com>
2016-07-19 12:16:55 +0800

    first commit

--- client/src/ui/user/Avatar.js
+++ client/src/ui/user/Avatar.js
@@ -9,14 +9,21 @@ import axios from 'axios';
 class Avatar extends Component {
   handleChange(e) {
     e.preventDefault();
+    //let API_URL = 'http://localhost:8000';//new
     let reader = new FileReader();
     let file = e.target.files[0];
     reader.onload = () => {
       this.props.uploadImage(reader.result, file.name);
     };
     reader.readAsDataURL(file);
+  
+  //new
+  //  axios.post(`${API_URL}/upload`, reader.reslut)   //将虚拟表单post 到  路径“第二行得到的URL／upload"
+  //  .then( response => {
+  //    this.setState({imageURL: API_URL + response.data.path.substr(6)});   //改变组件到state
+  //  });
+  //end
   }
-
   getStyles() {
     return {
       img: {
